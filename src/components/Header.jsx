be7d29dd2ba312e821d02d047305f5ad9896cccd
Jay McKerracher <jaymckerracher@hotmail.co.uk>
2023-12-12 10:02:49 +0000

    adding some general styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 function Header () {
     return (
-        <header>
+        <header className="app-header">
             <h1>NC News</h1>
             <img src="images/logo.png" alt="NC News Logo" id="logo-img"/>
         </header>
